bfb2cb346aee27dab2ed88922c67fc6507bca3a1
Sean Anderson <github@seananderson.ca>
2013-03-18 16:21:58 -0700

    place noise properly outside of bracket in Ricker

---
+++ R/ricker_ar1.R
@@ -0,0 +1,10 @@
+# Ricker model with AR1 error structure
+
+ricker_ar1 <- function(spawners, a, b, sigma_sq_v = 0, v_t_minus1 = 0, rho = 0) {
+  v_t <- rnorm(1, mean = 0, sd = sqrt(sigma_sq_v)) # process noise
+  v_t_ar1 <- v_t_minus1 * rho + v_t
+  recruits <- spawners * exp(a * (1 - spawners / b) + v_t_ar1)
+  list(recruits = recruits, eps = v_t_ar1)
+}
+
+
