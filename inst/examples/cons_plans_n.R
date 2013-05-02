0cf02fd805d49caaf09a32ec18563735ddcb50c4
Sean Anderson <github@seananderson.ca>
2013-05-01 17:43:33 -0700

    Remove linear-trend plot (by commenting out)

--- inst/examples/cons_plans_n.R
+++ inst/examples/cons_plans_n.R
@@ -9,38 +9,54 @@ w <- list()
 for(i in 1:length(num_pops)) {
   w[[i]] <- matrix(rep(1, num_pops[i]) * (tot_abund / num_pops[i]), nrow = 1)
 }
-
 plans_name <- num_pops
-w_show <- seq_len(length(num_pops))
+
+#### in this version, the pops are wiped out; total abundance changes
+w <- list()
+
+for(i in 1:4) {
+  w[[i]] <- matrix(rep(625, 16), nrow = 1)
+}
+  
+w[[1]][-c(1, 16)] <- 5 # 2 pops
+w[[2]][-c(1,2,15,16)] <- 5 # 4 pops
+w[[3]][-c(1:4,13:16)] <- 5 # 8 pops
+plans_name <- num_pops[1:4]
+####
+
+w_show <- seq_len(length(w))
 
 if(USE_CACHE) {
   load("plans_mv_n.rda")
 }else{
   plans_mv_arma_n <- list()
-  plans_mv_linear_n <- list()
-  for(i in 1:length(num_pops)) {
-    #plans_mv_arma_n[[i]] <- get_conserv_plans_mv(
-      #weights = w[[i]], 
-      #env_type = "arma",
-      #env_params = list(mean_value = 16, ar = 0.4, sigma_env = 1.4, ma = 0)
-      #)
-    plans_mv_linear_n[[i]] <- get_conserv_plans_mv(
+  #plans_mv_linear_n <- list()
+  for(i in 1:length(w)) {
+    plans_mv_arma_n[[i]] <- get_conserv_plans_mv(
       weights = w[[i]], 
-      env_type = "linear",
-      env_params = list(min_value = 10, max_value = 20, sigma_env = 0.2))
-    print(paste("Completed", i, "of", length(num_pops), "conservation plans to evaluate"))
+      reps = 30,
+      env_type = "arma",
+      env_params = list(mean_value = 16, ar = 0.4, sigma_env = 1.4, ma = 0)
+      )
+    #plans_mv_linear_n[[i]] <- get_conserv_plans_mv(
+      #weights = w[[i]], 
+      #reps = 30,
+      #env_type = "linear",
+      #env_params = list(min_value = 10, max_value = 20, sigma_env = 0.2))
+    print(paste("Completed", i, "of", length(w), "conservation plans to evaluate"))
   }
 
   # move to list of dataframes instead of list of list of dataframes
   # this is to match the typical output with even population numbers
   # across runs
-  plans_mv_arma_n_dfs <- list() 
+  #plans_mv_arma_n_dfs <- list() 
   plans_mv_linear_n_dfs <- list() 
-  for(i in 1:length(num_pops)) {
-    plans_mv_arma_n_dfs[[i]] <- plans_mv_arma_n[[i]][[1]]
+  for(i in 1:length(w)) {
+    #plans_mv_arma_n_dfs[[i]] <- plans_mv_arma_n[[i]][[1]]
     plans_mv_linear_n_dfs[[i]] <- plans_mv_linear_n[[i]][[1]]
   }
-  save(plans_mv_arma_n_dfs, plans_mv_linear_n_dfs, file = "plans_mv_n.rda")
+  #save(plans_mv_arma_n_dfs, plans_mv_linear_n_dfs, file = "plans_mv_n.rda")
+  save(plans_mv_arma_n_dfs, file = "plans_mv_n.rda")
 }
 
 source("plot_cons_plans.R")
