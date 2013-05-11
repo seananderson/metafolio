62869626c4a6fd8f324b59bf6822c1771341ce24
Sean Anderson <github@seananderson.ca>
2013-05-11 10:19:24 -0700

    Expand documentation for fit_ricker

--- R/fit_ricker.R
+++ R/fit_ricker.R
@@ -1,8 +1,14 @@
 #' Fit a Ricker curve
+#'
+#' @param S Spawners
+#' @param R Recruits or returns
 
-#S <- seq(100, 1000, length.out = 25)
-#R <- sapply(S, function(x) ricker_ar1(spawners = x, a = 1.9, b = 900, sigma_sq_v = 0.01)$recruits)
-#plot(S, log(R/S))
+#' @examples
+#' S <- seq(100, 1000, length.out = 25)
+#' R <- sapply(S, function(x) ricker_ar1(spawners = x, a = 1.9, b =
+#' 900, sigma_sq_v = 0.01)$recruits)
+#' plot(S, log(R/S))
+#' @export
 
 fit_ricker <- function(S, R) {
   m <- lm(log(R/S) ~ S)
