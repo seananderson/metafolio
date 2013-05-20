67f9e78813e069f85cf4c6f68825a60ff81eeb62
Sean Anderson <github@seananderson.ca>
2013-05-20 12:49:37 -0700

    Create README.r to build figures for paper

---
+++ inst/examples/README.r
@@ -0,0 +1,22 @@
+#' All steps to re-create the figures
+
+
+setwd("~/Dropbox/ms/metafolio/inst/examples/")
+devtools::load_all("../../")
+col_pal <- rev(gg_color_hue(10))
+
+# Figure show different number rules of thumb:
+source("cons_plans_n_random_resp_div.R")
+
+# Figure show different spatial rules of thumb:
+source("cons_plans2.R")
+
+# Figure showing thermal curves for spatial scenarios
+source("plot_thermal_curves_scenarios.R")
+
+
+# Supp Figures:
+source("plot_straying_matrix.R")
+source("plot_thermal_curves.R")
+source("example_returns_3.R")
+
