1a5bfbaee6dc6be85d5e7c9787087cc91f1ad795
Sean Anderson <github@seananderson.ca>
2013-05-24 15:23:04 -0700

    Add count_quasi_exts function

---
+++ R/count_quasi_exts.R
@@ -0,0 +1,34 @@
+#' Take \code{\link{meta_sim}} output objects and count quasi
+#' extinctions
+#'
+#' @param dat Input data. Should be a list of lists. The first level
+#' corresponds to the conservation plan and the second level
+#' corresponds to the replicate.
+#' @param quasi_thresh
+#' @param ignore_pops_thresh
+#' @param duration 
+#' @return
+#' A list of matrices. The list elements correspond to the
+#' conservation plans. The columns of the matrix correspond to the
+#' subpopulations that were above the \code{ignore_pops_thresh} level.
+#' The rows of the matrix correspond to the replicates.
+#' @export
+#' @examples \dontrun{
+#' count_quasi_exts(x_arma_sp$plans_port, quasi_thresh = 200)
+#'}
+
+count_quasi_exts <- function(dat, quasi_thresh, ignore_pops_thresh = 5, 
+  duration = 1) {
+  subpop_qe <- plyr::llply(x_arma_sp$plans_port, function(x) {
+    plyr::laply(x, function(y) {
+      conserved_pops <- which(y$A[1, ] > 5)
+      out <- apply(y$A[, conserved_pops], 2, function(z) {
+        temp <- is_quasi_ext(z, thresh = 200, duration = 1)$first_qe
+        temp
+      })
+      out
+    })
+  })
+  return(subpop_qe)
+}
+
