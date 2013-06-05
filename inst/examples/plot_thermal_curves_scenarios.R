64770585173b145eaa58dbbff015f10f854607fe
Sean Anderson <github@seananderson.ca>
2013-06-05 10:32:36 -0700

    Modify plot_thermal_curves_scenarios to add all possible curves

--- inst/examples/plot_thermal_curves_scenarios.R
+++ inst/examples/plot_thermal_curves_scenarios.R
@@ -1,7 +1,7 @@
 #' plot thermal curves for different conservation scenarios
 
-pdf("thermal-curve-scenarios.pdf", width = 3.0, height = 4.2)
-par(cex = 0.8, mgp = c(2.4, 0.55, 0), tck = -0.04, mfrow = c(4,1), mar = c(0,3,0,.5), oma = c(3, 0, .5, 0))
+pdf("thermal-curve-scenarios.pdf", width = 2.6, height = 4.7)
+par(cex = 0.8, mgp = c(2.4, 0.55, 0), tck = -0.04, mfrow = c(5,1), mar = c(0,3,0,.5), oma = c(3, 0, .5, 0))
     par(las = 1)
 
 optim_temps <- seq(13, 19, length.out = 10)
@@ -9,12 +9,14 @@ optim_temps <- seq(13, 19, length.out = 10)
 widths <- c(seq(0.05, 0.02, length.out = 5), rev(seq(0.05, 0.02, length.out = 5)))
 
 heights <- c(seq(2.8, 2.2, length.out = 5), rev(seq(2.8, 2.2, length.out = 5)))
+load("max_a_s.rda")
+heights <- max_a_s
 
 x <- seq(3, 29, length.out = 200)
 
 plot_the_curves <- function(ids) {
-  plot(1, 1, xlim = c(4, 28), ylim = c(-0.01, 2.9), ylab = "", xlab = "", type = "n", yaxs = "i", las = 1, xaxt = "n", yaxt = "n", axes = FALSE)
-  axis(2, at = seq(0, 2), col = "grey50")
+  plot(1, 1, xlim = c(4, 28), ylim = c(-0.01, 3.6), ylab = "", xlab = "", type = "n", yaxs = "i", las = 1, xaxt = "n", yaxt = "n", axes = FALSE)
+  axis(2, at = seq(0, 3), col = "grey50")
   box(col = "grey50")
   for(i in ids) {
     a <- thermal_curve_a(x, optim_temp = optim_temps[i], max_a = heights[i], width_param = widths[i])
@@ -22,15 +24,23 @@ plot_the_curves <- function(ids) {
     lines(x, a, col = col_pal[i], lwd = 1.5)
   }
 }
-al <- function(x) {text(3, 2.5, x, side = 3, line = -2, pos = 4)}
+
+bg.plot <- function(colour = "#00000019") rect(par("usr")[1],
+  par("usr")[3], par("usr")[2], par("usr")[4], col = colour, border =
+  FALSE)
+
+al <- function(x) {text(3, 3.20, x, pos = 4)}
+plot_the_curves(1:10) 
+al("(a) All possible tolerance curves")
+bg.plot()
 plot_the_curves(c(2, 4, 7, 9)) 
-al("(a) Full range")
+al("(b) Conserve full range")
 plot_the_curves(c(4, 5, 6, 7)) 
-al("(b) Most stable only")
+al("(c) Conserve most tolerant")
 plot_the_curves(c(1, 2, 3, 4)) 
-al("(c) One half")
+al("(d) Conserve one half")
 plot_the_curves(c(7, 8, 9, 10)) 
-al("(d) One half")
+al("(e) Conserve other half")
 #plot_the_curves(c(1, 2, 9, 10)) 
 axis(1, col = "grey50")
 par(xpd = NA)
