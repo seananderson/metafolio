e01aea3a104287c8a5116facf7d38f03620b6e5f
Sean Anderson <github@seananderson.ca>
2013-10-01 15:33:20 -0700

    Clean and document is_quasi_ext

--- R/is_quasi_ext.R
+++ R/is_quasi_ext.R
@@ -1,10 +1,14 @@
 #' Return whether there was an instance of quasi extinction
-#' 
+#'
 #' @param x A numeric vector.
 #' @param thresh The quasi-extinction threshold in absolute numbers.
 #' @param duration The number of years below the threshold before a
-#' quasi extinction has occured.
+#' quasi extinction has occurred.
 #' @export
+#' @examples
+#' x <- seq(100, 0, length.out = 20)
+#' is_quasi_ext(x, thresh = 10)
+
 is_quasi_ext <- function(x, thresh, duration = 1) {
   is_qe <- sum(x < thresh) > duration
   if(is_qe) {
@@ -14,5 +18,3 @@ is_quasi_ext <- function(x, thresh, duration = 1) {
   }
   list(is_qe = is_qe, first_qe = first_qe)
 }
-
-
