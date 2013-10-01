fd9410a5da7450484ae770f5489deab184ddcf38
Sean Anderson <github@seananderson.ca>
2013-10-01 13:04:40 -0700

    Make fit_ricker_fast the default

--- R/fit_ricker.R
+++ R/fit_ricker.R
@@ -1,37 +1,27 @@
-#' Fit a Ricker curve
+#' Fit a Ricker curve (quickly)
 #'
-#' @param S Spawners
-#' @param R Recruits or returns
-
+#' Fit a Ricker curve to spawner-recruit data and return the intercept
+#' (a) and slope (b).
+#' This function uses \code{\link{lm.fit()}} by directly
+#' specifying the model matrix. This is about an order of magnitude
+#' faster than \code{lm()}.
+#'
+#' @param S Spawners as a numeric vector
+#' @param R Recruits or returns as a numeric vector
+#' @export
+#' @return
+#' A named list with components \code{a} for the intercept and
+#' \code{b} for the slope.
 #' @examples
 #' S <- seq(100, 1000, length.out = 25)
 #' R <- sapply(S, function(x) ricker_ar1(spawners = x, a = 1.9, b =
 #' 900, sigma_sq_v = 0.01)$recruits)
 #' plot(S, log(R/S))
-#' @export
+#' fit_ricker(S, R)
 
 fit_ricker <- function(S, R) {
-  m <- lm(log(R/S) ~ S)
-  m.coef <- coef(m)
-  a <- as.numeric(m.coef[1])
-  b <- -as.numeric(a/m.coef[2])
-  return(list(a = a, b = b))
-}
-
-#' Fit a Ricker curve (quickly)
-#'
-#' This version uses \code{\link{stats::lm.fit}} by directly
-#' specifying the model matrix. This is about an order of magnitude
-#' faster than \code{\link{fit_ricker}}.
-#'
-#' @param S Spawners
-#' @param R Recruits or returns
-#' @export
-
-fit_ricker_fast <- function(S, R) {
   m <- lm.fit(x = cbind(rep(1, length(S)), S), y = log(R/S))
   a <- m$coefficients[[1]]
   b <- -a/m$coefficients[[2]]
   return(list(a = a, b = b))
 }
-
