1781db34337b71f2862d5cbc8c4d750d4a6ffaaa
Sean Anderson <github@seananderson.ca>
2013-10-01 13:12:19 -0700

    Update readme instructions

--- inst/examples/README.r
+++ inst/examples/README.r
@@ -1,8 +1,10 @@
-#' All steps to re-create the figures
+# All steps to re-create the figures
 
+# Set your R working directory to this folder (the examples folder)
 
-setwd("~/Dropbox/ms/metafolio/inst/examples/")
-devtools::load_all("../../")
+# install and load the package or run:
+# devtools::load_all("../../")
+
 col_pal <- rev(gg_color_hue(10))
 
 # Figure show different number rules of thumb:
@@ -14,9 +16,7 @@ source("cons_plans2.R")
 # Figure showing thermal curves for spatial scenarios
 source("plot_thermal_curves_scenarios.R")
 
-
 # Supp Figures:
 source("plot_straying_matrix.R")
 source("plot_thermal_curves.R")
 source("example_returns_3.R")
-
