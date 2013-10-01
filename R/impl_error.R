2ed0a193c6206aad6a85622c8879a5f6fb3e1965
Sean Anderson <github@seananderson.ca>
2013-10-01 15:02:01 -0700

    Clean up and document implemention error fxns

--- R/impl_error.R
+++ R/impl_error.R
@@ -1,25 +1,45 @@
-#' Add implementation error
-
-# From Pestes et al. 2008:
-
-#portion harvested between 0 and 1:
-#Hact,yr = beta(βh, γh), (1)
-#where values of βh and γh were the beta distribution’s lo- cation and shape parameters, with βh = Htar,yr for a given year and γh = 0.1 (the latter determined by numerical ex- periment to approximate the observed implementation uncertainty).
-
-impl_error <- function(mu, sigma_impl) {
-  var <- sigma_impl ^ 2
-
+#' Get beta parameters from mean and variance
 est_beta_params <- function(mu, var) {
-# http://stats.stackexchange.com/questions/12232/calculating-the-parameters-of-a-beta-distribution-using-the-mean-and-variance
   alpha <- ((1 - mu) / var - 1 / mu) * mu ^ 2
   beta <- alpha * (1 / mu - 1)
-  return(list(alpha = alpha, beta = beta))
+  list(alpha = alpha, beta = beta)
 }
 
-bp <- est_beta_params(mu, var)
-#print(bp)
-#hist(rbeta(10000, bp$alpha, bp$beta))
-#rbeta(1, bp$alpha, bp$beta)
-mapply(rbeta, n = 1, shape1 = bp$alpha, shape2 = bp$beta)
+#' Add implementation error
+#'
+#' Add implementation error with a beta distribution.
+#'
+#' @param mu The mean
+#' @param sigma_impl Implementation error standard deviation
+#' @export
+#' @return
+#' A single numeric values representing a sample from a beta
+#' distribution with the specified mean and standard deviation.
+#'
+#' @references
+#' Morgan, M. G. & Henrion, M. (1990). Uncertainty: A Guide to Dealing
+#' with Uncertainty in Quantitative Risk and Policy Analysis.
+#' Cambridge University Press.
+#'
+#' Pestes, L. R., Peterman, R. M., Bradford, M. J., and Wood, C. C.
+#' (2008). Bayesian decision analysis for evaluating management
+#' options to promote recovery of a depleted salmon population.
+#' 22(2):351-361.
+#'
+#' http://stats.stackexchange.com/questions/12232/calculating-the-parameters-of-a-beta-distribution-using-the-mean-and-variance
+#' @examples
+#' y <- sapply(1:200, function(x) impl_error(0.5, 0.2))
+#' hist(y)
+#'
+#' y <- sapply(1:200, function(x) impl_error(0.3, 0.1))
+#' hist(y)
+
+
+
+impl_error <- function(mu, sigma_impl) {
+  if(mu > 1 | mu < 0) stop("mu must be between 0 and 1")
+  var <- sigma_impl ^ 2
+  bp <- est_beta_params(mu, var)
+  mapply(rbeta, n = 1, shape1 = bp$alpha, shape2 = bp$beta)
 }
 
