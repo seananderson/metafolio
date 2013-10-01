555056d797a7a4aaadd8671154d733043978fcde
Sean Anderson <github@seananderson.ca>
2013-10-01 15:02:11 -0700

    Document get_port_vals

--- R/get_port_vals.R
+++ R/get_port_vals.R
@@ -1,7 +1,21 @@
-#' Get portfolio mean and variance values from a meta_sim() object 
-#' 
-#' @param x A list object as returned from \code{meta_sim}
-#' @param burn Number of years to throw out at burn in
+#' Get portfolio mean and variance values
+#'
+#' Takes a list created by \code{\link{meta_sim}} and returns the mean
+#' and variance values. This function is used by other internal
+#' functions, but can also be used as its own low-level function.
+#'
+#' @param x A list object as returned from \code{\link{meta_sim}}
+#' @param burn Number of years to throw out as burn in
+#' @export
+#' @seealso \code{\link{get_conserv_plans_mv}},
+#' \code{\link{plot_cons_plans}}
+#' @return A data frame with columns for the mean (m) and variance
+#' (v).
+#' @examples
+#' arma_env_params <- list(mean_value = 16, ar = 0.1, sigma_env = 2, ma = 0)
+#' base1 <- meta_sim(n_pop = 10, env_params = arma_env_params, env_type =
+#'   "arma", assess_freq = 5)
+#' get_port_vals(base1)
 
 get_port_vals <- function(x, burn = 1:30) {
     port.x <- rowSums(x$A[-burn, ])
