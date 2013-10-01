1d1c86981401d22784b110a33e63fb381bf53777
Sean Anderson <github@seananderson.ca>
2013-10-01 15:28:21 -0700

    Clean and document (and add e.g.) for run_cons_plans()

--- R/run_cons_plans.R
+++ R/run_cons_plans.R
@@ -8,7 +8,7 @@
 #' spatial strategies of conservation, conserving different numbers of
 #' populations, and a lack of knowledge. You can do this by how you
 #' set your \code{w} weight object. See the example.
-#' 
+#'
 #' @param w A (nested) list of weights. The first list level contains the
 #' different plans. The next level contains repetitions for a given
 #' plan. E.g. \code{cp[[2]][[1]]} contains the first iteration of the
@@ -30,35 +30,59 @@
 #' represents a trial run. Within \code{plans_port}, each
 #' first level of the list contains a weight element and each
 #' second level of the list contains a replicate.
-#' @examples \dontrun{
-#' ## in this version, the pops are wiped out; total abundance changes
-#' n_trials <- 20 # number of trials at each n conservation plan
-#' n_plans <- 4 # number of plans
+#' @examples
+#' \dontrun{
+#' set.seed(1)
+#' w_plans <- list()
+#' w_plans[[1]] <- c(5, 1000, 5, 1000, 5, 5, 1000, 5, 1000, 5)
+#' w_plans[[2]] <- c(5, 5, 5, 1000, 1000, 1000, 1000, 5, 5, 5)
+#' w_plans[[3]] <- c(rep(1000, 4), rep(5, 6))
+#' w_plans[[4]] <- rev(w_plans[[3]])
+#' plans_name_sp <- c("Full range of responses", "Most stable only",
+#' "Lower half", "Upper half")
+#'  n_trials <- 50 # number of trials at each n conservation plan
+#'  n_plans <- 4 # number of plans
+#'  num_pops <- c(2, 4, 8, 16) # n pops to conserve
+#'  w <- list()
+#'  for(i in 1:n_plans) { # loop over number conserved
+#'   w[[i]] <- list()
+#'   for(j in 1:n_trials) { # loop over trials
+#'     w[[i]][[j]] <- matrix(rep(625, 16), nrow = 1)
+#'     w[[i]][[j]][-sample(1:16, num_pops[i])] <- 5
+#'   }
+#'  }
+#' arma_env_params <- list(mean_value = 16, ar = 0.1, sigma_env = 2, ma = 0)
+#'
+#' x_arma_sp <- run_cons_plans(w, env_type = "arma", env_params = arma_env_params)
+#'
+#' plot_cons_plans(x_arma_sp$plans_mv, plans_name = plans_name_sp, cols =
+#'   cols, add_all_efs = FALSE, xlim = c(0.02, 0.15), ylim = c(-0.017,
+#'     0.017), add_legend = FALSE)
+#' }
+#'
+#' # In this version, the pops are wiped out; total abundance changes
+#' n_trials <- 50 # number of trials at each n conservation plan
 #' num_pops <- c(2, 4, 8, 16) # n pops to conserve
+#' n_plans <- length(num_pops) # number of plans
 #' w <- list()
 #' for(i in 1:n_plans) { # loop over number conserved
 #'  w[[i]] <- list()
 #'  for(j in 1:n_trials) { # loop over trials
-#'    w[[i]][[j]] <- matrix(rep(625, 16), nrow = 1)
-#'    w[[i]][[j]][-sample(1:16, num_pops[i])] <- 5 # conserve
-#'    num_pops[i] populations; wipe out rest
+#'    w[[i]][[j]] <- matrix(rep(1000, 16), nrow = 1)
+#'    w[[i]][[j]][-sample(1:16, num_pops[i])] <- 5
 #'  }
 #' }
-#' 
-#' ## ARMA:
-#' x <- run_cons_plans(w, env_type = "arma", env_params =
-#' list(mean_value = 16, ar = 0.4, sigma_env = 1.4, ma = 0))
+#' plans_name_n <- paste(num_pops, "populations")
+#' arma_env_params <- list(mean_value = 16, ar = 0.1, sigma_env = 2, ma = 0)
 #'
-#' cols <- RColorBrewer::brewer.pal(5, "Spectral")
-#' # may look funny because we only have 20 trials in this example:
-#' plot_cons_plans(x, plans_name = num_pops, cols = cols, add_all_efs
-#' = TRUE)
-#' 
-#' ## Linear:
-#' x <- run_cons_plans(w, env_type = "linear", env_params =
-#' list(min_value = 11.5, max_value = 20.5, sigma_env = 0.1, start_t =
-#' 31)) 
+#' x_arma_n <- run_cons_plans(w, env_type = "arma", env_params =
+#'   arma_env_params, max_a = thermal_integration(16))
+#'
+#' plot_cons_plans(x_arma_n$plans_mv, plans_name = plans_name_n, cols =
+#'   cols, add_all_efs = FALSE, xlim = c(0.02, 0.15), ylim = c(-0.017,
+#'     0.017), add_legend = FALSE)
 #' }
+
 run_cons_plans <- function(w, env_type, env_params, show_progress =
   TRUE, burn = 1:30, assess_freq = 5, ...) {
 
@@ -91,7 +115,7 @@ run_cons_plans <- function(w, env_type, env_params, show_progress =
   # So, the structure will be plans_port_n[[w]][[trial]]
   plans_port_n_out <- list()
   for(i in 1:length(w)) {
-    plans_port_n_out[[i]] <- list() 
+    plans_port_n_out[[i]] <- list()
     for(j in 1:n_trials) {
       plans_port_n_out[[i]][[j]] <- plans_port_n[[i]][[j]][[1]][[1]]
     }
@@ -100,9 +124,9 @@ run_cons_plans <- function(w, env_type, env_params, show_progress =
   # Move to list of dataframes instead of list of list of dataframes
   # this is to match the typical output with even population numbers
   # across runs.
-  plans_mv_n_dfs <- list() 
+  plans_mv_n_dfs <- list()
   for(i in 1:length(w)) {
-    plans_mv_n_dfs[[i]] <- list() 
+    plans_mv_n_dfs[[i]] <- list()
     for(j in 1:n_trials) {
       plans_mv_n_dfs[[i]][[j]] <- plans_mv_n[[i]][[j]][[1]]
     }
@@ -114,4 +138,3 @@ run_cons_plans <- function(w, env_type, env_params, show_progress =
   return(list(plans_mv = plans_mv_n_dfs, plans_port =
       plans_port_n_out))
 }
-
