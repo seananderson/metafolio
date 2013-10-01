7dade60a1fc12ebb4c047a30392eaa1234160813
Sean Anderson <github@seananderson.ca>
2013-10-01 13:30:11 -0700

    Add documentation and clean code on cor plot fxn

--- R/plot_correlation_between_returns.R
+++ R/plot_correlation_between_returns.R
@@ -1,26 +1,36 @@
 #' Plot correlation of returns across stocks.
-#' 
-#' @param x A list output object from \code{meta_sim}.
-#' @param burn Number of years to discard at start as burnin.
-#' @param pal Colours to plot with for stocks.
+#'
+#' Create a matrix plot showing the correlation between the returns of
+#' each stock/asset.
+#'
+#' @param x A list output object from \code{\link{meta_sim}}.
+#' @param burn Number of years to discard at start as burn in.
+#' @param pal Colours to label each stock/asset.
+#' @examples
+#' arma_env_params <- list(mean_value = 16, ar = 0.1, sigma_env = 2, ma = 0)
+#' base1 <- meta_sim(n_pop = 10, env_params = arma_env_params, env_type =
+#'   "arma", assess_freq = 5)
+#' plot_correlation_between_returns(base1)
 #' @export
 
 plot_correlation_between_returns <- function(x, burn = 1:30, pal = rev(gg_color_hue(x$n_pop))) {
-  # x is output from a simulation run
 
 par(mfrow = c(10, 10), cex = 0.5, mar = c(0,0,0,0), oma = c(4, 4, 0, 0))
-for(i in 1:10) {
-  for(j in 1:10) {
-    if(j < i){ plot(x$A[-burn, i], x$A[-burn, j], log = "xy", axes = FALSE, pch = 20, col = "grey20")
+for (i in 1:10) {
+  for (j in 1:10) {
+    if (j < i){ plot(x$A[-burn, i], x$A[-burn, j], log = "xy", axes =
+      FALSE, pch = 20, col = "grey20")
     box(col = "grey50")
-    }else{ 
+    }else{
       plot(1,1, type = "n", xlab = "", ylab = "", axes = FALSE)
     }
-    if(i == 10) mtext(j, side = 1, line = 1, cex = 0.8, col = pal[j])
-    if(j == 1) mtext(i, side = 2, las = 1, line = 1, cex = 0.8, col = pal[i])
+    if (i == 10) mtext(j, side = 1, line = 1, cex = 0.8, col = pal[j])
+    if (j == 1) mtext(i, side = 2, las = 1, line = 1, cex = 0.8, col = pal[i])
   }
 }
-    mtext("log of returns by subpopulation", side = 1, line = 2.5, cex = 0.8, outer = TRUE)
-    mtext("log of returns by subpopulation", side = 2, line = 2.5, cex = 0.8, outer = TRUE, las = 0)
+    mtext("log of returns by subpopulation", side = 1, line = 2.5, cex
+      = 0.8, outer = TRUE)
+    mtext("log of returns by subpopulation", side = 2, line = 2.5, cex
+      = 0.8, outer = TRUE, las = 0)
 
 }
