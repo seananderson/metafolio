a226b0609dcb191dfcbe39a19831a3e36ec0fef5
Sean Anderson <github@seananderson.ca>
2013-10-01 14:45:21 -0700

    Clean up ricker_escapement

--- R/ricker_escapement.R
+++ R/ricker_escapement.R
@@ -1,10 +1,13 @@
 #' Assign a salmon escapement target based on a Ricker curve
-#' 
-#' @details
-#' Set escapement according to Hilborn and Walters (1992) p272, Table 7.2. Smsy = b(0.5 - 0.07*a).
+#'
+#' Sets escapement according to Hilborn and Walters (1992) p272, Table
+#' 7.2. Smsy = b(0.5 - 0.07*a).
+#'
 #' @param a Ricker productivity parameter.
 #' @param b Ricker density-dependent parameter.
-#' @references Hilborn, R.W. and Walters, C. 1992. Quantitative fisheries stock assessment: Choice, dynamics, and uncertainty. Chapman and Hall, London.
+#' @references Hilborn, R.W. and Walters, C. 1992. Quantitative
+#' fisheries stock assessment: Choice, dynamics, and uncertainty.
+#' Chapman and Hall, London.
 #' @export
 #' @examples
 #' ricker_escapement(1.1, 1000)
