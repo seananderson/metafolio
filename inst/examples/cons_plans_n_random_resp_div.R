2928c79391dbb611a3c994ec0b6dcf9421d9f2ba
Sean Anderson <github@seananderson.ca>
2013-10-01 13:05:33 -0700

    Move add_inset_env out of package
    Don't want to make user install TeachingDemos
    Would rather not re-code

--- inst/examples/cons_plans_n_random_resp_div.R
+++ inst/examples/cons_plans_n_random_resp_div.R
@@ -6,6 +6,17 @@
 set.seed(1)
 USE_CACHE <- TRUE
 
+# moved this out of the package to avoid Teaching Demos import
+add_inset_env <- function(env, x = 0.12, y = -0.013, size = c(1, .5), ...) {
+  TeachingDemos::subplot(plot(seq(0.1, 0.15, length.out =
+        length(env)), env, axes = FALSE, ann = FALSE, type =
+      "l", col = "grey10"), x = x, y = y, size = size, ...)
+  par(xpd = NA)
+  text(x, y, "Environment", adj = c(.5, 3.0), col = "black", cex = 1)
+  par(xpd = FALSE)
+}
+
+
 # in this version, the pops are wiped out; total abundance changes
 n_trials <- 800 # number of trials at each n conservation plan
 num_pops <- c(2, 4, 8, 16) # n pops to conserve
