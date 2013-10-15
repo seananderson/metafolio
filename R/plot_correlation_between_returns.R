650de2c98d0c9c8d75d7d997ccf5e8368c93278a
Sean Anderson <github@seananderson.ca>
2013-10-15 13:15:42 -0700

    Fix labels in correlation plot

--- R/plot_correlation_between_returns.R
+++ R/plot_correlation_between_returns.R
@@ -1,7 +1,7 @@
-#' Plot correlation of returns across stocks.
+#' Plot correlation of returns (i.e. metapopulation abundance) across stocks.
 #'
-#' Create a matrix plot showing the correlation between the returns of
-#' each stock/asset.
+#' Create a matrix plot showing the correlation between the log
+#' returns of each stock/asset.
 #'
 #' @param x A list output object from \code{\link{meta_sim}}.
 #' @param burn Number of years to discard at start as burn in.
@@ -13,7 +13,9 @@
 #' plot_correlation_between_returns(base1)
 #' @export
 
-plot_correlation_between_returns <- function(x, burn = 1:30, pal = rev(gg_color_hue(x$n_pop))) {
+plot_correlation_between_returns <- function(x, burn = 1:30, pal =
+  rev(gg_color_hue(x$n_pop)), xlab = "log of return abundance by
+  population", ylab = "log of return abundance by population") {
 
 par(mfrow = c(10, 10), cex = 0.5, mar = c(0,0,0,0), oma = c(4, 4, 0, 0))
 for (i in 1:10) {
@@ -28,9 +30,7 @@ for (i in 1:10) {
     if (j == 1) mtext(i, side = 2, las = 1, line = 1, cex = 0.8, col = pal[i])
   }
 }
-    mtext("log of returns by subpopulation", side = 1, line = 2.5, cex
-      = 0.8, outer = TRUE)
-    mtext("log of returns by subpopulation", side = 2, line = 2.5, cex
-      = 0.8, outer = TRUE, las = 0)
+    mtext(xlab, side = 1, line = 2.5, cex = 0.8, outer = TRUE)
+    mtext(ylab, side = 2, line = 2.5, cex = 0.8, outer = TRUE, las = 0)
 
 }
