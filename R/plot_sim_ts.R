44fb3be2aa4b73568b03b9540f2bceb4255635ec
Sean Anderson <github@seananderson.ca>
2013-10-26 14:06:08 -0700

    Revamp plot_sim_ts to be more pretty

--- R/plot_sim_ts.R
+++ R/plot_sim_ts.R
@@ -1,20 +1,40 @@
 #' Add annotations to panel
-annotate <- function(text, adj = 0.05) {
-  mtext(text, side = 3, line = -1.2, cex = 0.7, adj = adj)
+annotate <- function(label, xfrac = 0.008, yfrac = 0.18, pos = 4, cex = 0.9, ...) {
+  u <- par("usr")
+  x <- u[1] + xfrac * (u[2] - u[1])
+  y <- u[4] - yfrac * (u[4] - u[3])
+  text(x, y, label, pos = pos, cex = cex, ...)
 }
 
 #' Add a pretty axis
-my.axis <- function(side, shade_years = NULL, ylab = "") {
+my.axis <- function(side, shade_years = NULL, ylab = "", yticks = NA) {
   if(!is.null(shade_years)) {
     rect(min(shade_years), -100, max(shade_years), 1e9, col =
       "#00000030", border = NA)
   }
   #axis(side, col = "grey50")
-  axis(side, col = "grey50", at = pretty(axTicks(2), n= 2))
+  if(is.na(yticks[1])) {
+    axis(side, col = "grey50", at = pretty(axTicks(2), n= 2))
+  } else {
+    axis(side, col = "grey50", at = yticks)
+  }
   par(las = 0)
   mtext(ylab, side = 2, cex = 0.7, line = 3)
   par(las = 1)
 }
+
+
+#' Standard matrix plot of values by stream for one panel:
+plot_panel_lines <- function(dat, ymin = c("zero", "min"), ystretch = 1.1, ...) {
+  if(ymin[1] == "zero")
+    ylim <- c(0, max(dat) * ystretch)
+  if(ymin[1] == "min")
+    ylim <- c(min(dat), max(dat) * ystretch)
+  matplot(dat, type = "l", lty = 1, xlab =
+    "Time", xaxt = "n", axes = FALSE, xaxs = "i", ylim = ylim , ...)
+  box(col = "grey50")
+}
+
 
 #' Plot various time series from a simulation run
 #'
@@ -40,84 +60,75 @@ my.axis <- function(side, shade_years = NULL, ylab = "") {
 #' plot_sim_ts(base1)
 
 plot_sim_ts <- function(x, pal = rev(gg_color_hue(x$n_pop)),
-  years_to_show = 30, burn = 1:50, shade_years = NULL, adj = 0.02) {
+  years_to_show = 30, burn = 1:50, shade_years = NULL, adj = 0.02,
+  add_units = FALSE, yticks = NA) {
+
+  if(!add_units) {
+    ylabs <- rep("", 10)
+  } else {
+    ylabs <- c(rep("Value", 2), rep("#", 5), rep("Value", 3))
+  }
+
 
   # years to show in time series example plots
   to_show <- (max(burn)):(max(burn)+years_to_show)
 
   par(mfrow = c(10, 1), mar = c(0,0,0,0), oma = c(4, 4.5, 1, 1), cex =
     0.7, las = 1, xpd = FALSE)
-  par(tck = -0.03, mgp = c(2, .5, 0))
+  par(tck = -0.04, mgp = c(2, .45, 0))
 
   # environmental signal
-  plot(x$env_ts[to_show], type = "l", xaxs = "i", axes = FALSE)
+  plot(x$env_ts[to_show], ylim = c(min(x$env_ts[to_show]),
+      max(x$env_ts[to_show]) * 1.15), type = "l", xaxs = "i", axes =
+    FALSE)
   box(col = "grey50")
   abline(h = 0, lty = 2, lwd = 1.5, col = "grey50")
   annotate("Environmental signal", adj = adj)
-  my.axis(2, shade_years = shade_years, ylab = "Value")
+  my.axis(2, shade_years = shade_years, ylab = ylabs[1], yticks = yticks[[1]])
 
   # Ricker a values:
-  matplot(x$A_params[to_show, ], type = "l", col = pal, lty = 1, ylab =
-    "Returns", xlab = "Time", axes = FALSE, xaxs = "i", ylim = c(0,
-      max(x$A_params[to_show, ]) * 1.25))
-  box(col = "grey50")
-  my.axis(2, shade_years = shade_years, ylab = "Value")
+  plot_panel_lines(x$A_params[to_show, ], col = pal, ystretch = 1.3)
+  my.axis(2, shade_years = shade_years, ylab = ylabs[2], yticks = yticks[[2]])
   annotate("Productivity parameter", adj = adj)
 
   # returns:
-  matplot(x$A[to_show, ], type = "l", col = pal, lty = 1, ylab =
-    "Returns", xlab = "Time", xaxt = "n", axes = FALSE, xaxs = "i")
+  plot_panel_lines(x$A[to_show, ], col = pal)
   annotate("Returns", adj = adj)
-  box(col = "grey50")
-  my.axis(2, shade_years = shade_years, ylab = "#")
+  my.axis(2, shade_years = shade_years, ylab = ylabs[3], yticks = yticks[[3]])
 
   # catch
-  matplot(x$F[to_show, ], type = "l", col = pal, lty = 1, ylab =
-    "Returns", xlab = "Time", axes = FALSE, xaxs = "i")
-  box(col = "grey50")
-  my.axis(2, shade_years = shade_years, ylab = "#")
+  plot_panel_lines(x$F[to_show, ], col = pal)
+  my.axis(2, shade_years = shade_years, ylab = ylabs[4], yticks = yticks[[4]])
   annotate("Fisheries catch", adj = adj)
 
   # escapement
-  matplot(x$E[to_show, ], type = "l", col = pal, lty = 1, ylab =
-    "Returns", xlab = "Time", axes = FALSE, xaxs = "i")
-  box(col = "grey50")
-  my.axis(2, shade_years = shade_years, ylab = "#")
+  plot_panel_lines(x$E[to_show, ], col = pal)
+  my.axis(2, shade_years = shade_years, ylab = ylabs[5], yticks = yticks[[5]])
   annotate("Escapement", adj = adj)
 
   # strays leaving
-  matplot(x$Strays_leaving[to_show, ], type = "l", col = pal, lty = 1,
-    ylab = "Returns", xlab = "Time", axes = FALSE, xaxs = "i")
-  box(col = "grey50")
-  my.axis(2, shade_years = shade_years, ylab = "#")
+  plot_panel_lines(x$Strays_leaving[to_show, ], col = pal)
+  my.axis(2, shade_years = shade_years, ylab = ylabs[6], yticks = yticks[[6]])
   annotate("Strays leaving", adj = adj)
 
   # strays joining
-  matplot(x$Strays_joining[to_show, ], type = "l", col = pal, lty = 1,
-    ylab = "Returns", xlab = "Time", axes = FALSE, xaxs = "i")
-  box(col = "grey50")
-  my.axis(2, shade_years = shade_years, ylab = "#")
+  plot_panel_lines(x$Strays_joining[to_show, ], col = pal)
+  my.axis(2, shade_years = shade_years, ylab = ylabs[7], yticks = yticks[[7]])
   annotate("Strays joining", adj = adj)
 
   # SR residuals
-  matplot(x$Eps[to_show, ], type = "l", col = pal, lty = 1, ylab =
-    "Returns", xlab = "Time", axes = FALSE, xaxs = "i")
-  box(col = "grey50")
+  plot_panel_lines(x$Eps[to_show, ], ymin = "min", col = pal, ystretch = 1.4)
   abline(h = 0, lty = 2, lwd = 1.5, col = "grey50")
-  my.axis(2, shade_years = shade_years, ylab = "Value")
+  my.axis(2, shade_years = shade_years, ylab = ylabs[8], yticks = yticks[[8]])
   annotate("Spawner-return residuals", adj = adj)
 
-  matplot(x$Est_a[to_show, ], type = "l", col = pal, lty = 1, ylab =
-    "Estimated a", xlab = "", axes = FALSE, xaxs = "i")
-  box(col = "grey50")
+  plot_panel_lines(x$Est_a[to_show, ], ymin = "min", col = pal)
   annotate("Estimated a", adj = adj)
-  my.axis(2, shade_years = shade_years, ylab = "Value")
+  my.axis(2, shade_years = shade_years, ylab = ylabs[9], yticks = yticks[[9]])
 
-  matplot(x$Est_b[to_show, ], type = "l", col = pal, lty = 1, ylab =
-    "Estimated b", xlab = "", axes = FALSE, xaxs = "i")
-  box(col = "grey50")
+  plot_panel_lines(x$Est_b[to_show, ], ymin = "min", col = pal, ystretch = 1.4)
   annotate("Estimated b", adj = adj)
-  my.axis(2, shade_years = shade_years, ylab = "Value")
+  my.axis(2, shade_years = shade_years, ylab = ylabs[10], yticks = yticks[[10]])
 
   axis(1, col = "grey50")
   mtext("Generation", side = 1, outer = TRUE, line = 2.0, cex = 0.8)
