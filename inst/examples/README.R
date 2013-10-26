e1f122ccb1e15ab2f5229ce200fab6a9c1fd2d97
Sean Anderson <github@seananderson.ca>
2013-10-26 11:17:19 -0700

    Clarify comments in README.R

--- inst/examples/README.R
+++ inst/examples/README.R
@@ -1,22 +1,27 @@
 # All steps to re-create the figures
 
-# Set your R working directory to this folder (the examples folder)
+# Set your R working directory to this folder (the examples folder).
 
-# install and load the package or run:
-# devtools::load_all("../../")
+# install and load the package:
+# devtools::install_github("metafolio", "seananderson")
+# library(metafolio)
 
+# or, for rapid development:
+devtools::load_all("../../")
+
+# ggplot like colour palette to use throughout for stream ID:
 col_pal <- rev(gg_color_hue(10))
 
-# Figure show different number rules of thumb:
+# Figure showing different number rules of thumb:
 source("cons_plans_n_random_resp_div.R")
 
-# Figure show different spatial rules of thumb:
+# Figure showing different spatial rules of thumb:
 source("cons_plans2.R")
 
-# Figure showing thermal curves for spatial scenarios
+# Figure showing thermal curves for spatial scenarios:
 source("plot_thermal_curves_scenarios.R")
 
-# Supp Figures:
+# Supplementary Figures:
 source("plot_straying_matrix.R")
 source("plot_thermal_curves.R")
 source("example_returns_3.R")
