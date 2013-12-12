cea202e3b257d117f64ab9e0d4d36939562b888e
Sean Anderson <github@seananderson.ca>
2013-12-11 19:22:30 -0800

    Clean up readme

--- inst/examples/README.R
+++ inst/examples/README.R
@@ -2,17 +2,22 @@
 
 # Set your R working directory to this folder (the examples folder).
 
-# install and load the package:
-# devtools::install_github("metafolio", "seananderson")
-# library(metafolio)
+# Install and load the package and then:
+library(metafolio)
 
 # or, for rapid development:
-devtools::load_all("../../")
+# devtools::load_all("../../")
 
 # ggplot like colour palette to use throughout for stream ID:
 col_pal <- rev(gg_color_hue(10))
 
-# Figure showing different number rules of thumb:
+# Warning: these first two sourced files will take a while to run.
+# Once they've be run once, you can go in and turn USE_CACHE <- TRUE
+# near the top of each file if you want to use the previously
+# generated output.
+#
+# Figure showing random response diversity but different
+# numbers of populations conserved:
 source("cons_plans_n_random_resp_div.R")
 
 # Figure showing different spatial rules of thumb:
