e080ae326cc8123d805471e33203e236ca954eb8
Sean Anderson <github@seananderson.ca>
2013-12-12 15:43:12 -0800

    Tidy up figs2eps.R

--- inst/examples/figs2eps.R
+++ inst/examples/figs2eps.R
@@ -1,8 +1,8 @@
 f <- list.files(pattern = "*.pdf")
-# these have transparency, need to use AI
+# these have transparency, need to use Illustrator to flatten:
 f <- f[-which(f == "spatial-mv.pdf")]
-f <- f[-which(f == "plot-various-options-ts-3pops.pdf")]
 f <- f[-which(f == "cons-plans-n.pdf")]
+
 for(i in 1:length(f)) {
   system(paste0("pdftops -eps ", f[i]))
 }
