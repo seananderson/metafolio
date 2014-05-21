3fdd21fff2262eb89aecdc6e755ff6861b97dc92
Sean Anderson <github@seananderson.ca>
2014-05-21 15:08:48 -0700

    Remove NAs for ts plotting, update example plot

--- R/plot_sim_ts.R
+++ R/plot_sim_ts.R
@@ -46,6 +46,7 @@ my.axis <- function(side, shade_years = NULL, ylab = "", yticks = NA) {
 #' axis limits. This is useful to make space for a panel label within the plot.
 #' @param ... Anything else to pass to \code{\link[graphics]{matplot}}.
 plot_panel_lines <- function(dat, ymin = c("zero", "min"), ystretch = 1.1, ...) {
+  dat[is.na(dat)] <- 0
   if(ymin[1] == "zero")
     ylim <- c(0, max(dat) * ystretch)
   if(ymin[1] == "min")
@@ -78,8 +79,8 @@ plot_panel_lines <- function(dat, ymin = c("zero", "min"), ystretch = 1.1, ...) 
 #' @examples
 #' arma_env_params <- list(mean_value = 16, ar = 0.1, sigma_env = 2, ma = 0)
 #' base1 <- meta_sim(n_pop = 10, env_params = arma_env_params, env_type =
-#'   "arma", assess_freq = 5)
-#' plot_sim_ts(base1)
+#'   "arma", assess_freq = 5, decrease_b = 10)
+#' plot_sim_ts(base1, years_to_show = 70, burn = 1:30)
 
 plot_sim_ts <- function(x, pal = rev(gg_color_hue(x$n_pop)),
   years_to_show = 30, burn = 1:50, shade_years = NULL, adj = 0.02,
